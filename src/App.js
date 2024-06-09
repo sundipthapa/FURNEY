ea173ce591bdf85a0f0371511b39ffa555ce289e
sundipthapa <lakhanmgr123@gmail.com>
2024-06-09 10:54:50 +0545

    Adding routes

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 import Login from './components/Auth/Login';
 import SignUp from './components/Auth/SignUp';
 import AddToCartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
 import FaqPage from './pages/FaqPage';
 import FavoritePage from './pages/FavoritePage';
 import LandingPage from './pages/LandingPage';
 import PostProduct from './pages/Postpage';
 import ProductCollection from './pages/ProductCollectionPage';
 import ProductDescriptionPage from './pages/ProductDescriptionPage';
+import ProfilePage from './pages/ProfilePage';
 export default function App() {
   return (
     <Router>
@@ -26,6 +28,8 @@ export default function App() {
         <Route path='/productdescription' element={<ProductDescriptionPage />} />
         <Route path='/cart' element={<AddToCartPage />} />
         <Route path='/favorite' element={<FavoritePage />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
         <Route path='/faq' element={<FaqPage />} />
 
       </Routes>
